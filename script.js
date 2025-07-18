77b62643e4db0fe447100687458bde0d9a36450d
Rafael Sousa <rafaelfsousa06@gmail.com>
2025-07-18 14:55:33 +0100

    Some changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const CLIENT_ID = '25371662123-opqktsrvje4ab91s0i9e4lt0bgvmo1g2.apps.googleusercontent.com';
+const CLIENT_ID = '731258606855-s027dr73u4a1p7r4449321oq5onq4isv.apps.googleusercontent.com';
 const SCOPES = 'https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.readonly';
 let accessToken;
 let directoryHandle;
